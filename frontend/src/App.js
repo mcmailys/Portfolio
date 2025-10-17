1f9a728d2b67be5f2cd6e79dff7c7a7fba04015a
Mailys <mailysmakane@gmail.com>
2025-10-17 23:11:57 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
-- import { BrowserRouter, Routes, Route } from "react-router-dom";
-+ import { HashRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
--   <BrowserRouter basename="/Portfolio">
-+   <HashRouter>
+    <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
       </div>
--   </BrowserRouter>
-+   </HashRouter>
+    </Router>
   );
 }
+
+export default App;
